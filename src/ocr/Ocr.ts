0a0ccc244d9e4d3424404cc7aaa26dcd63cc53ef
Gilbert Champion <gchampion@liveperson.com>
2019-01-25 15:04:23 -0500

    Add Chinese

--- src/ocr/Ocr.ts
+++ src/ocr/Ocr.ts
@@ -3,14 +3,14 @@ import tesseract from 'node-tesseract-ocr';
 export async function getText(imagePath: string, userWordsPath: string) {
   const configuration = {
     'user-words': userWordsPath,
-    lang: 'eng',
+    lang: 'eng+chi_sim',
     oem: 1,
     psm: 12,
     // presets: ['bazaar'],
   };
   let text: string = (await tesseract.recognize(imagePath, configuration));
   // text = text.replace(new RegExp('\n', 'g'), '');
-  text = text.replace(/[\r\n]+/g,'');
+  text = text.replace(/[\r\n]+/g, '');
   console.log(`Found text: ${JSON.stringify(text)}`);
   return text;
 }
